c5c1566a4350e2b0de95769d0cafefc68c8ac329
SanjanaBH <89254025+SanjanaBH@users.noreply.github.com>
2022-10-09 19:25:08 +0530

    Add files via upload

--- connection.java
+++ connection.java
@@ -1,22 +1,59 @@
 import java.sql.*;
 
 public class connection {
-    public static void main(String[] args) throws Exception {
+
+    public static String getTableData(String tableName) throws SQLException, ClassNotFoundException {
+        Connection con = null;
+        Statement st = null;
+        ResultSet rs = null;
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
-            Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/world", "root", "root");
+            con = DriverManager.getConnection("jdbc:mysql://localhost:3306/world", "root", "root");
             Statement stmt = con.createStatement();
-            ResultSet rs = stmt.executeQuery("select * from cuslogin");
-            System.out.println("ID" + "\t\t" + "Name" + "\t\t" + "Email");
-            System.out.println(
-                    "------------------------------------------------------------------------------------------------------");
+            rs = stmt.executeQuery("select * from " + tableName);
+
+            ResultSetMetaData rsmd = rs.getMetaData();
+            int colCount = rsmd.getColumnCount();
+
+            StringBuilder b = new StringBuilder("<CustomerDetails>\n");
+
             while (rs.next()) {
-                System.out.println(rs.getString(1) + "\t\t" + rs.getString(2) + "\t\t" + rs.getString(3));
-                System.out.println("------------------------------------------------------------------------------------------------------");
+                b.append("<Customer>");
+                for (int i = 1; i <= colCount; i++) {
+                    String columnName = rsmd.getColumnName(i);
+                    b.append('<').append(columnName).append('>');
+                    b.append(rs.getObject(i));
+                    b.append("</").append(columnName).append('>');
+                }
+                b.append("</Customer>\n");
             }
-            con.close();
-        } catch (Exception e) {
-            System.out.println(e);
+            b.append("</CustomerDetails>");
+            return b.toString();
+        } catch (SQLException e) {
+            throw e;
+        } catch (ClassNotFoundException e) {
+            throw e;
+        } finally {
+            if (rs != null)
+                try {
+                    rs.close();
+                } catch (SQLException e) {
+                }
+            if (st != null)
+                try {
+                    st.close();
+                } catch (SQLException e) {
+                }
+            if (con != null)
+                try {
+                    con.close();
+                } catch (SQLException e) {
+                }
         }
+    }
+
+    public static void main(String[] args) throws Exception {
+        String str = getTableData("cuslogin");
+        System.out.println(str);
     }
 }
