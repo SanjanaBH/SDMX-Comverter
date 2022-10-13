2875a7cf4ba80457c92dae76ee2d0cb170a3c0d0
SanjanaBH <89254025+SanjanaBH@users.noreply.github.com>
2022-10-13 11:47:51 +0530

    Add files via upload

---
+++ my_update.java
@@ -0,0 +1,28 @@
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+public class my_update{
+	
+void  my_db_update(String str1, String str2,String str3,String str4) {
+		try{  
+	Class.forName("com.mysql.jdbc.Driver");  
+	Connection con=DriverManager.getConnection(  
+	"jdbc:mysql://localhost:3306/my_tutorial","root","password");  
+	Statement st=con.createStatement();  
+	int mark = Integer.parseInt(str3); // Mark is an integer
+	// Adding record 
+	String query1="INSERT INTO `my_tutorial`.`student`"
+	 + " (`name`, `class`, `mark`, `sex`)"
+	 + "VALUES('" +str1+"','"+str2+"',"+mark+",'"+str4+"')";
+	st.executeUpdate(query1); // record added. 
+	
+	con.close();  
+	
+	}catch(Exception e){ System.out.println(e);} 
+	//////////////////////////////
+}
+}
+
